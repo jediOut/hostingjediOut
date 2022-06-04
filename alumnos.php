d23695bc518b22450dfc52cc578a03e606d5edbd
jediOut <91569234+jediOut@users.noreply.github.com>
2022-06-04 10:21:14 -0500

    Create alumnos.php

---
+++ alumnos.php
@@ -0,0 +1,9 @@
+<?php
+try {
+  $pdo = new PDO("mysql:host=localhost;dbname=id17753206_wp_5097745a0577eb6364ddfc77cde29f05", 'id17753206_wp_5097745a0577eb6364ddfc77cde29f05', '}a91T}1NH}}hf[mz');
+  //echo "Connected successfully";
+} catch(PDOException $e) {
+  print "¡Error!: " . $e->getMessage() . "<br/>";
+  die();
+}
+?>
